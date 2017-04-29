03e07b45bb80368ef850c9a46769e5309c3d23ca
Felix Jung <felix@jung.fm>
2017-04-29 13:43:12 +0200

    Hook up app to firebase via angularfire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { ElasticModule } from 'angular2-elastic';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { DraftComponent } from './draft/draft.component';
 import { DraftslistComponent } from './draftslist/draftslist.component';
 import { DraftActionsComponent } from './draft-actions/draft-actions.component';
@@ -13,7 +18,6 @@ import { TitleComponent } from './title/title.component';
 import { tinydraftRoutes } from './app.routes';
 import { EditorComponent } from './editor/editor.component';
 import { DraftsService } from './service/drafts.service';
-import { ElasticModule } from 'angular2-elastic';
 import {
   DraftResolve,
   FavedDraftsResolve,
@@ -22,6 +26,7 @@ import {
 } from './service/drafts.resolve';
 import { DraftDetailComponent } from './draft-detail/draft-detail.component';
 import { LoginComponent } from './login/login.component';
+import { firebaseConfig } from './firebase.config';
 
 @NgModule({
   declarations: [
@@ -41,6 +46,8 @@ import { LoginComponent } from './login/login.component';
     RouterModule.forRoot(tinydraftRoutes),
     FlexLayoutModule,
     ElasticModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
   ],
   providers: [
     DraftResolve,
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDOeE_fAgz3vPtDhCQ0Q1SgnsUspyI4jk4',
+  authDomain: 'tinydraft-1be9f.firebaseapp.com',
+  databaseURL: 'https://tinydraft-1be9f.firebaseio.com',
+  projectId: 'tinydraft-1be9f',
+  storageBucket: 'tinydraft-1be9f.appspot.com',
+  messagingSenderId: '515051549736'
+};
